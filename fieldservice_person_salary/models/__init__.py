1926e9b4bd7c5268233a0e05d2b361005cc5f1bd
LuPinheiro <gerallupinheiro@gmail.com>
2023-04-13 10:55:30 +0200

    [ADD] fieldservice_person_salary
    Replaces workers link to FSM Orders to an extended record with persons
    and salary

---
+++ fieldservice_person_salary/models/__init__.py
@@ -0,0 +1,2 @@
+from . import fsm_order_salary
+from . import fsm_order
---
+++ fieldservice_person_salary/models/fsm_order_salary.py
@@ -0,0 +1,41 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields, api
+
+
+class FSMOrderSalary(models.Model):
+    _name = "fsm.order.salary"
+    _description = "Field Service Order Salary"
+
+    # TODO fill this field // use display_name if needed
+    name = fields.Char()
+    fsm_order_id = fields.Many2one('fsm.order', required=True)
+    sale_id = fields.Many2one("sale.order")
+    fsm_person_id = fields.Many2one('fsm.person', required=True)
+    sequence = fields.Integer()
+    vehicle = fields.Boolean()
+    payments_salary = fields.Monetary(string='Salary', required=True)
+    payments_km = fields.Monetary(string='Km')
+    payments_diets = fields.Monetary(string='Diets')
+    payments_advance = fields.Monetary(string='Advance')
+    net_salary = fields.Monetary(currency_field='currency_id',
+        compute='_compute_net_salary',
+        string='Net Salary', store=True)
+    currency_id = fields.Many2one('res.currency', 'Currency', required=True,
+        default=lambda self: self.env.company.currency_id.id)
+    scheduled_date_start = fields.Datetime(
+        related='fsm_order_id.scheduled_date_start',
+        store=True)
+    location_id = fields.Many2one(
+        "fsm.location", 
+        related='fsm_order_id.location_id',
+        string="Location", 
+        index=True, 
+        store=True)
+    customer_id = fields.Many2one('res.partner', string="Contact")
+
+
+    @api.depends('payments_salary','payments_km','payments_diets','payments_advance')
+    def _compute_net_salary (self):
+        for person in self:
+            person.net_salary = (person.payments_salary + person.payments_km + person.payments_diets - person.payments_advance)
