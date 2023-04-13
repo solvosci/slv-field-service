18a919a0f678653843097cf0644eb453093a0dd4
David Alonso // Solvos <david.alonso@solvos.es>
2023-04-13 13:27:49 +0200

    [ADD] fieldservice_person_salary_sale_reference
    Adds sale reference to Person Salary for FSM orders.

---
+++ fieldservice_person_salary_sale_reference/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "FieldService Person Salary - Sale Reference link",
+    "summary": """
+        Adds sale reference to Person Salaty for FSM orders
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Field Service",
+    "website": "https://github.com/solvosci/slv-field-service",
+    "depends": ["fieldservice_person_salary", "fieldservice_sale_reference"],
+    "data": ["views/fsm_order_salary_views.xml"],
+    "installable": True,
+}
