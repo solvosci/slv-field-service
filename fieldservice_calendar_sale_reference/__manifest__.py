cf1f3910f0fc0f67be58f5998d8a1f337266d7bd
LuPinheiro <gerallupinheiro@gmail.com>
2023-05-02 09:13:33 +0200

    [ADD] fieldservice_calendar_sale_reference

---
+++ fieldservice_calendar_sale_reference/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "FieldService Calendar Sale Reference",
+    "summary": """
+        Concatenate order name with customer sales order reference in Calendar Events.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Field Service",
+    "website": "https://github.com/solvosci/slv-field-service",
+    "depends": [ "fieldservice_sale_reference", "fieldservice_calendar"],
+    'installable': True,
+}
