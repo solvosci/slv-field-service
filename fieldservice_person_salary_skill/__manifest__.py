c7f4eee015c82ab970a6280259448cce20b55ca9
LuPinheiro <gerallupinheiro@gmail.com>
2023-05-03 12:11:04 +0200

    [IMP] fieldservice_person_salary_skill: hide skills in the salary view"

--- fieldservice_person_salary_skill/__manifest__.py
+++ fieldservice_person_salary_skill/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Field Service",
     "website": "https://github.com/solvosci/slv-field-service",
     "depends": ["fieldservice_person_salary", "fieldservice_skill"],
